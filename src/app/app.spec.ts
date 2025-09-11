6a90aeed453114b4f8fdf0423a6519e002a4f066
Valentin Wissler <valentinwissler42@outlook.com>
2025-09-11 12:19:32 +0100

    tests working

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,23 +1,65 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { provideRouter } from "@angular/router";
+import { provideLocationMocks } from "@angular/common/testing";
 
 describe('App', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [App],
-    }).compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [
+                App,
+            ],
+            providers: [
+                provideRouter([]),
+                provideLocationMocks()
+            ]
+        }).compileComponents();
+    });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    it('should create the app', () => {
+        const fixture = TestBed.createComponent(App);
+        const app = fixture.componentInstance;
+        expect(app).toBeTruthy();
+    });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, timeline-ui');
-  });
+    it('should have a router outlet', () => {
+        const fixture = TestBed.createComponent(App);
+        const compiled = fixture.nativeElement as HTMLElement;
+        expect(compiled.querySelector('router-outlet')).not.toBeNull();
+    })
+
+    it('should render title', () => {
+        const fixture = TestBed.createComponent(App);
+        fixture.detectChanges();
+        const compiled = fixture.nativeElement as HTMLElement;
+        expect(compiled.querySelector('h1')?.textContent).toContain('Hello timeline-ui');
+    });
+
+    it('should have a navigation bar', () => {
+        const fixture = TestBed.createComponent(App);
+        fixture.detectChanges();
+        const compiled = fixture.nativeElement as HTMLElement;
+        expect(compiled.querySelector('nav')).not.toBeNull();
+    })
+
+    it('should have a link to home', () => {
+        const fixture = TestBed.createComponent(App);
+        fixture.detectChanges();
+        const compiled = fixture.nativeElement as HTMLElement;
+        expect(compiled.querySelector('a[routerLink=""]')).not.toBeNull();
+    })
+
+    it('should have a link to dashboard', () => {
+        const fixture = TestBed.createComponent(App);
+        fixture.detectChanges();
+        const compiled = fixture.nativeElement as HTMLElement;
+        expect(compiled.querySelector('a[routerLink="dashboard"]')).not.toBeNull();
+    })
+
+    it('should have a link to settings', () => {
+        const fixture = TestBed.createComponent(App);
+        fixture.detectChanges();
+        const compiled = fixture.nativeElement as HTMLElement;
+        expect(compiled.querySelector('a[routerLink="settings"]')).not.toBeNull();
+    })
 });
